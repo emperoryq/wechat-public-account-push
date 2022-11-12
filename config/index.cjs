370e65e489a79b6d845a762382803e5e1cb3421c
emperoryq <45561719+emperoryq@users.noreply.github.com>
2022-11-12 16:50:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17802T1DUXSnhoQ3ulnjF3QxwK38Np4p7FqZua',
+      id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -59,7 +59,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU17802T1DUXSnhoQ3ulnjF3QxwK38Np4p7FqZua',
+      id: '',
     }
   ],
 
